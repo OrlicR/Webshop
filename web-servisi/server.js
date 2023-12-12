147354ce36a55f89f88e1b4b7a30bb257e0a337f
OrlicR <robertorlic2000@gmail.com>
2023-12-12 17:46:02 +0100

    poruka

--- web-servisi/server.js
+++ web-servisi/server.js
@@ -2,46 +2,60 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.get("/podaci", function(request, response){
-    return response.send({messasge:"ok"});
-})
 
-app.get("/podaci/:id", function(request, response){
-    var id = request.params.id;
-    return response.send({messasge: id+ " sve je ok"});
-})
+//const dbConfig = require("./db.config.js");
+var mysql = require('mysql');
+var dbConn = mysql.createConnection({ 
+    host: "localhost", 
+    user: "root", 
+    password: "", 
+    database: "pzi"
+ });
+dbConn.connect(); //konekcija na bazu
 
-app.post("/podaci", function(request, response){
-    var podaci = request.body.podatak;
-    return response.send({message:" sve je ok"});
-})
+app.get('/korisnik', function (request, response) { 
+    dbConn.query('SELECT * FROM korisnik', function (error, results, fields) { 
+        if (error) throw error; 
+        return response.send({ error: false, data: results, message: 'READ Svi korisnici.' }); 
+    }); 
+});
+
+app.get('/korisnik/:id', function (request, response) { 
+    let useful_part_id = request.params.id; 
+    if (!useful_part_id) {
+    return response.status(400).send({ error: true, message: 'Molim vas upišite korisnikov ID' }); 
+    }
+    dbConn.query('SELECT * FROM useful_part where id=?', useful_part_id, function (error, results, fields){
+    if (error) throw error; 
+    return response.send({C}); 
+    });
+});
 
 app.post("/korisnik", function(request, response){
     var ime = request.body.ime;
     var prezime = request.body.prezime;
-
-    return response.send({korisnik: "CREATE "+ime+" " +prezime});
+    var tel = request.body.tel;
+    dbConn.query('INSERT INTO korisnik VALUES(NULL,?,?,?)',[ime, prezime, tel], function (error, results, fields) { 
+        if (error) throw error; 
+        return response.send({error: false, data: results, message: 'INSERT korisnik ime='+ime});
+    }); 
 })
 
 app.put("/korisnik/:id", function(request, response){
     var id = request.params.id;
-    var adresa = request.body.adresa;
-    return response.send({korisnik: "UPDATE "+id+" nova adresa:" +adresa});
+    var tel = request.body.tel;
+    dbConn.query('UPDATE korisnik SET tel=? WHERE id=?',[tel, id], function (error, results, fields) { 
+        if (error) throw error; 
+        return response.send({error: false, data: results, message: 'UPDATE korisnik id='+id+ ' tel'+tel});
+    }); 
 })
 
 app.delete("/korisnik/:id", function(request, response){
     var id = request.params.id;
-    return response.send({korisnik: "DELETE "+id});
-})
-
-
-app.get("/korisnik/", function(request, response){
-    return response.send({korisnik: "READ "});
-})
-
-app.get("/korisnik/:id", function(request, response){
-    var id = request.params.id;
-    return response.send({korisnik: "READ "+id});
+    dbConn.query('DELETE FROM korisnik WHERE id=?',id, function (error, results, fields) { 
+        if (error) throw error; 
+        return response.send({error: false, data: results, message: 'DELETE korisnik id='+id}); 
+    }); 
 })
 
 // set port
